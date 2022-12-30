3cfe19bc1ba7c167da651258566ce3b734ac9ed4
TDB <attila.kisivan@gmail.com>
2022-12-30 10:02:26 +0000

    created Add item

--- script.js
+++ script.js
@@ -98,10 +98,33 @@ function updateDOM() {
 
 };
 
+// Add to column list
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM();
+}
+
+// Show add item input box
+function showInputBox(column) {
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+};
+
+// Hide item input box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+};
+
+
 //Allows arrays to reflect drag and drop items
 function rebuildArrays(){
-  console.log(backlogList.children);
-  console.log(progressList.children);
   backlogListArray=[];
   for(let i = 0; i < backlogList.children.length; i++) {
     backlogListArray.push(backlogList.children[i].textContent);
@@ -125,7 +148,6 @@ function rebuildArrays(){
 // When item start dragging
 function drag(e) {
   draggedItem = e.target;
-  console.log('draggedItem', draggedItem);
 }
 
 // Column allows for iem to drop
